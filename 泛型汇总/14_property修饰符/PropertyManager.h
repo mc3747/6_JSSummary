f6e939a5190f2645ed7d5b6415f533004508eeed
mc3747 <gjfax@macheng.local>
2018-06-12 15:03:02 +0800

    180612_新增关键字

--- 泛型汇总/14_property修饰符/PropertyManager.h
+++ 泛型汇总/14_property修饰符/PropertyManager.h
@@ -23,7 +23,7 @@
     assign: 进行赋值（基本类型，c语言类型，id类型）
     weak：引用计数器不变化，也不持有对象，对象消失后，指针自动变成nil（delegate的代理属性，UI控件）。
     strong：引用计数器会+1。
-    copy: 建立一个引用计数为1的对象，（NSString和block）
+    copy: 建立一个引用计数为1的对象，（NSString，NSArray，NSDictionary，和block，）
  
     retain: MRC中用retain，ARC中用strong。
     unsafe_unretained：和weak类似，但是引用计数为0，变量不会置为nil
